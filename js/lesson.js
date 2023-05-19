383d88792ba605cd6d487b060d3ba6f130bc81aa
YOWASHI <129309849+yowashi-is-fish@users.noreply.github.com>
2023-05-19 22:26:01 +0900

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -3,8 +3,7 @@ var itemRadios = document.getElementsByName("lesson");
 for (var i = 0; i < itemRadios.length; i++) {
 	itemRadios[i].addEventListener("change", function() {
 
-    var itemValue = this.value;
-    var itemElement = document.getElementById(itemValue);
+    var itemElement = document.getElementById(this.value);
     var itemElements = document.getElementsByClassName("lesson");
 
     for (var j = 0; j < itemElements.length; j++) {
